1696fde306d9224591f1ff5f7bbc0bf0c5a71ba9
VitorCorrea18 <vitorcorrea18@gmail.com>
2022-04-27 19:48:53 -0400

    feat: requisito 4 - validacoes do endpoint /login (done)

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,15 +1,49 @@
 const crypto = require('crypto');
-const { STATUS_OK } = require('../utils/statusCode');
+const {
+  HTTP_STATUS_OK,
+  HTTP_STATUS_BAD_REQUEST,
+  REQUIRED_EMAIL_MESSAGE,
+  REQUIRED_PWD_MESSAGE,
+  INVALID_EMAIL_MESSAGE,
+  INVALID_PWD_MESSAGE,
+} = require('../utils/statusCode');
 
-const login = (req, res) => {
-  // const data = req.headers;
+const PWD_MIN_LENGTH = 6;
 
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(HTTP_STATUS_BAD_REQUEST).json(REQUIRED_EMAIL_MESSAGE);
+  }
+
+  if (!email.includes('@') || !email.includes('.com')) {
+    return res.status(HTTP_STATUS_BAD_REQUEST).json(INVALID_EMAIL_MESSAGE);
+  }
+  next();
+};
+
+const validatePwd = (req, res, next) => {
+  const pwd = req.body.password;
+
+  if (!pwd) {
+    return res.status(HTTP_STATUS_BAD_REQUEST).json(REQUIRED_PWD_MESSAGE);
+  }
+
+  if (pwd.length < PWD_MIN_LENGTH) {
+    return res.status(HTTP_STATUS_BAD_REQUEST).json(INVALID_PWD_MESSAGE);
+  }
+  next();
+};
+
+const login = (__req, res) => {
   // Metodo crype.randomBytes retirado da documentação oficial do Node.js: 
   // https://nodejs.org/api/crypto.html#cryptorandombytessize-callback 
   const token = { token: crypto.randomBytes(8).toString('hex') };
-  return res.status(STATUS_OK).send(token);
+  return res.status(HTTP_STATUS_OK).send(token);
 };
 
 module.exports = {
   login,
+  validateEmail,
+  validatePwd,
 };
